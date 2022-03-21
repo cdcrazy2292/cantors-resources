d88c7976616fa366357c30f91eabb6db4a0bf00f
cristhiansoria <crisso2292@gmail.com>
2022-03-21 02:17:15 -0400

    first commit

---
+++ src/components/Songs/models/songs-model.js
@@ -0,0 +1,98 @@
+export const Songs = [
+    {
+        name: "Exultet",
+        description: "",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "Oh Lord Our God",
+        description: "",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "Akeda",
+        description: "",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "Song of Moses",
+        description: "",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "Song of Tobit",
+        description: "",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "Shema Israel",
+        description: "",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "Shout for Joy",
+        description: "",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "What is Different",
+        description: "Song of the Children",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "Glory to God",
+        description: "",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "Paschal Alleluia",
+        description: "",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "How good It Is",
+        description: "",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "There were Two Angels",
+        description: "Second Song of the Children",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "Lamb of god",
+        description: "",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "Homily of Melito",
+        description: "",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "To the Paschal Victim",
+        description: "",
+        img: "",
+        sourceUrl: ""
+    },
+    {
+        name: "Dayenu",
+        description: "",
+        img: "",
+        sourceUrl: ""
+    }
+]
